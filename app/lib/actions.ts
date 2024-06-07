0af932f0d8b7575a434cbc96a83d1b4ac7770a7c
Cheryl Brady <cbrady80@byui.edu>
2024-06-07 11:13:12 -0600

    Ch15 - added auth

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/app/auth';
+import { AuthError } from 'next-auth';
  
 const FormSchema = z.object({
   id: z.string(),
@@ -117,4 +119,25 @@ export async function createInvoice(prevState: State, formData: FormData) {
       };
     }
     
+  }
+
+  export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+  ) {
+    try {
+      // console.log(formData);
+      await signIn('credentials', formData);
+    } catch (error) {
+      console.log(error);
+      if (error instanceof AuthError) {
+        switch (error.type) {
+          case 'CredentialsSignin':
+            return 'Invalid credentials.';
+          default:
+            return 'Something went wrong.';
+        }
+      }
+      throw error;
+    }
   }
